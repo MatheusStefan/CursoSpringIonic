eb64d9f948085704182624e1c7af231a9a7132f7
Matheus Stefan <roccomatheus0@gmail.com>
2021-10-14 11:25:56 -0300

    Começando a implementar a autenticação

--- src/interceptors/error-interceptor.ts
+++ src/interceptors/error-interceptor.ts
@@ -1,8 +1,6 @@
 import { Injectable } from "@angular/core";
 import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HTTP_INTERCEPTORS } from "@angular/common/http";
 import { Observable } from "rxjs/RX";
-import { analyzeAndValidateNgModules } from "@angular/compiler";
-import { catchError } from "rxjs/operators";
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {

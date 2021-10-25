bb4e54b87aa632bbc8d9ccf9d89d6d84e700a2f7
Matheus Stefan <roccomatheus0@gmail.com>
2021-10-25 17:06:17 -0300

    Salvando pedido

--- src/pages/order-confirmation/order-confirmation.ts
+++ src/pages/order-confirmation/order-confirmation.ts
@@ -5,6 +5,7 @@ import { ClienteDTO } from '../../models/cliente.dto';
 import { EnderecoDTO } from '../../models/endereco.dto';
 import { PedidoDTO } from '../../models/pedido.dto';
 import { CartService } from '../../services/domain/cart.service';
+import { PedidoService } from '../../services/domain/categoria.service copy';
 import { ClienteService } from '../../services/domain/cliente.service';
 
 @IonicPage()
@@ -23,7 +24,8 @@ export class OrderConfirmationPage {
     public navCtrl: NavController, 
     public navParams: NavParams,
     public cartService: CartService,
-    public clienteService: ClienteService) {
+    public clienteService: ClienteService,
+    public pedidoService: PedidoService) {
 
       this.pedido = this.navParams.get('pedido');
   }
@@ -45,7 +47,24 @@ export class OrderConfirmationPage {
     return list[posicion];
   }
   
+  back() {
+    this.navCtrl.setRoot('CartPage');
+  }
+
   total() {
     return this.cartService.total();
+  }
+
+  checkout() {
+    this.pedidoService.insert(this.pedido)
+      .subscribe(response => {
+        this.cartService.createOrClearCart();
+        console.log(response.headers.get('location'));
+      },
+      error => {
+        if(error.status == 403) {
+          this.navCtrl.setRoot('HomePage');
+        }
+      })
   }
 }
